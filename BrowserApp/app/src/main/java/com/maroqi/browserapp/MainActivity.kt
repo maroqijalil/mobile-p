51a36441b2861e91c3d3034eaae9faaedbe490ca
maroqijalil <jalilpro31@gmail.com>
2021-11-09 16:59:25 +0700

    feat: fix browser app

--- BrowserApp/app/src/main/java/com/maroqi/browserapp/MainActivity.kt
+++ BrowserApp/app/src/main/java/com/maroqi/browserapp/MainActivity.kt
@@ -5,8 +5,12 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.webkit.WebViewClient
 import android.widget.SearchView
 import com.maroqi.browserapp.databinding.ActivityMainBinding
+import android.webkit.WebView
+import androidx.core.view.MenuItemCompat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,18 +20,59 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    binding.mainSrl.setOnRefreshListener {
+      setupWeb()
+    }
+
+    setupWeb()
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
     menuInflater.inflate(R.menu.menu_main, menu)
 
-    (menu?.findItem(R.id.menu_search)?.actionView as SearchView).apply {
+    val searchView = menu?.findItem(R.id.menu_search)?.actionView as SearchView
+
+    searchView.apply {
       setSearchableInfo(
         (getSystemService(Context.SEARCH_SERVICE) as SearchManager)
           .getSearchableInfo(componentName)
       )
     }
 
+    searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+      override fun onQueryTextSubmit(query: String?): Boolean {
+        if (query != null) {
+          if (query.startsWith("http://") || query.startsWith("https://")) {
+            setupWeb(query)
+          } else {
+            setupWeb("http://$query")
+          }
+        }
+        return false
+      }
+
+      override fun onQueryTextChange(query: String?): Boolean {
+        return false
+      }
+    })
+
     return true
+  }
+
+  private fun setupWeb(url: String = "https://www.google.com/") {
+    binding.mainSrl.isRefreshing = true
+
+    binding.mainWv.apply {
+      settings.apply {
+        javaScriptEnabled = true
+      }
+      loadUrl(url)
+      webViewClient = object : WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+          binding.mainSrl.isRefreshing = false
+        }
+      }
+    }
   }
 }
