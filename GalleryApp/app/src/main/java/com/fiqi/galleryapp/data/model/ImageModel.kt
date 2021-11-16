6b8b8152e376cef4945433dae50b65d9725bc196
finesaaa <indriatifiqey16@gmail.com>
2021-11-17 06:27:17 +0700

    feat: bind data to view

--- GalleryApp/app/src/main/java/com/fiqi/galleryapp/data/model/ImageModel.kt
+++ GalleryApp/app/src/main/java/com/fiqi/galleryapp/data/model/ImageModel.kt
@@ -6,7 +6,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class ImageModel(
-  @Exclude
+  @get:Exclude
   var id: String = "",
   var title: String = "",
   var link: String = "",
--- GalleryApp/app/src/main/java/com/fiqi/galleryapp/view/adapters/main/ImageListViewHolder.kt
+++ GalleryApp/app/src/main/java/com/fiqi/galleryapp/view/adapters/main/ImageListViewHolder.kt
@@ -1,12 +1,23 @@
 package com.fiqi.galleryapp.view.adapters.main
 
+import android.net.Uri
 import androidx.recyclerview.widget.RecyclerView
+import com.fiqi.galleryapp.R
 import com.fiqi.galleryapp.databinding.ItemImageBinding
 import com.fiqi.galleryapp.data.model.ImageModel
+import com.squareup.picasso.Picasso
 
 class ImageListViewHolder(private val binding: ItemImageBinding) :
   RecyclerView.ViewHolder(binding.root) {
   fun bind(data: ImageModel) {
-
+    Picasso.get()
+      .load(data.link)
+      .resize(360, 360)
+      .centerCrop()
+      .placeholder(R.drawable.ic_baseline_image_24)
+      .error(R.drawable.ic_baseline_image_24)
+      .into(binding.itemIvImage)
+    binding.itemTvTitle.text = data.title
+    binding.itemTvDesc.text = data.desc
   }
 }
